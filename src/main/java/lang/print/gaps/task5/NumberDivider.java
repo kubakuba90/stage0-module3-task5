14bb6a5daf98f02bebe62c1145b584f4f0202ccf
Jakub M <fotograflubin@gmail.com>
2022-07-05 20:32:36 +0200

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        double divided = (double)toBeDivided / 5;
+        System.out.println(divided);
     }
 }
